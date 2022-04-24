7bd45caa25639ce79c57bdd58ecd40f3da3d63af
gmldnjs26 <imleesky265@gmail.com>
2022-04-24 16:37:39 +0900

    chore: usage

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -20,15 +20,32 @@
       src="https://placeimg.com/150/200/any"
       alt="sample image"
     />
+
+    <BookThumbnailLink
+      src="https://placeimg.com/150/200/any"
+      alt="sample image"
+      to="/category"
+      title="악의 마음을 읽는 자들asdㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ"
+    />
+    <BookThumbnailLink
+      size="large"
+      src="https://placeimg.com/150/200/any"
+      alt="sample image"
+      to="/category"
+      title="유괴의 날"
+      author="정해연 저"
+    />
   </div>
 </template>
 
 <script>
 import BookThumbnail from '../components/BookThumbnail.vue'
+import BookThumbnailLink from '../components/top/BookThumbnailLink.vue'
 
 export default {
   components: {
     BookThumbnail,
+    BookThumbnailLink,
   },
 }
 </script>
