b1761bbbe49249d51ad1dff0eedcbe3a992a8de3
AppKim <jskim2301@yahoo.co.jp>
2022-05-15 18:27:13 +0900

    add myselect component

--- frontend/components/selection/SelectionThumbnail.vue
+++ frontend/components/selection/SelectionThumbnail.vue
@@ -1,9 +1,15 @@
 <template>
-<div>SelectionThumbnail</div>
+    <book-thumbnail
+    size="xLarge"
+    src="https://placeimg.com/150/200/any"
+    alt="sample image"
+    ></book-thumbnail>
 </template>
 
 <script>
+import BookThumbnail from '../BookThumbnail.vue'
 export default {
+  components: { BookThumbnail },
 
 }
 </script>
--- frontend/pages/selection/_id.vue
+++ frontend/pages/selection/_id.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        Selection
+        <selection-thumbnail></selection-thumbnail>
     </div>
 </template>
 
 <script>
+import SelectionThumbnail from '../../components/selection/SelectionThumbnail.vue'
 export default {
+  components: { SelectionThumbnail },
 
 }
 </script>
