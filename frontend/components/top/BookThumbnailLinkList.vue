cbaab4df8eeb516f144851c1954c3e3dd3358387
gmldnjs26 <imleesky265@gmail.com>
2022-05-15 16:59:55 +0900

    feat: create book-thumbnail-link-list component

---
+++ frontend/components/top/BookThumbnailLinkList.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul class="book-thumbnail-link-list">
+    <li v-for="(book, i) in bookList" :key="i" class="book-thumbnail-link-list__item">
+      <BookThumbnailLink
+        :size="size"
+        :src="book.src"
+        :alt="book.alt"
+        :to="book.link"
+        :title="book.title"
+        :author="book.author"
+        :loading="loading"
+      ></BookThumbnailLink>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import BookThumbnailLink from './BookThumbnailLink.vue'
+
+export default defineComponent({
+  components: {
+    BookThumbnailLink,
+  },
+  props: {
+    bookList: {
+      type: Array,
+      default: () => [],
+    },
+    size: {
+      type: String,
+      default: 'medium',
+    },
+    loading: {
+      type: String,
+      default: 'lazy',
+    },
+  },
+  setup() {},
+})
+</script>
+<style lang="scss" scoped>
+.book-thumbnail-link-list {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow: auto;
+  &__item {
+    &:not(:last-child) {
+      margin-right: 12px;
+    }
+    &:nth-of-type(n + 7) {
+      display: none;
+    }
+    @include sp_view {
+      &:nth-of-type(n + 7) {
+        display: block;
+      }
+    }
+  }
+}
+</style>
