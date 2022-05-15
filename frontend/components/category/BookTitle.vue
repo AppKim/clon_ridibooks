cee54821abf5479bdb491dbd58e11299b81e4bc6
Nahjisu <nahjisoo0122@gmail.com>
2022-05-15 17:19:05 +0900

    make components

---
+++ frontend/components/category/BookTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Mypage />
+  </div>
+</template>
+
+<script>
+import Mypage from '@/components/mypage/Mypage.vue'
+export default {
+  comments: {
+    Mypage,
+  },
+}
+</script>
+
+<style></style>
