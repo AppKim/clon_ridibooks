664af60a18f7069fcab24948b1a8e6f0fc655891
AppKim <jskim2301@yahoo.co.jp>
2022-06-12 18:34:29 +0900

    solve conflict

---
+++ frontend/components/BookReview.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="review">
+    <div class="review__star">
+      <span class="star5_rating" :data-rate="rate" />
+      <span class="review__count">{{ score }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    rate: {
+      type: Number,
+      defalut: 0,
+      required: true,
+    },
+    score: {
+      type: Number,
+      defalut: 0,
+      required: true,
+    },
+  },
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped>
+.star5_rating {
+  position: relative;
+  z-index: 0;
+  display: inline-block;
+  white-space: nowrap;
+  color: #cccccc;
+  width: 100%;
+  font-size: 12px;
+  &:before {
+    content: '★★★★★';
+  }
+  &:after {
+    content: '★★★★★';
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    white-space: nowrap;
+    color: #ffcf32;
+  }
+  &[data-rate='5']:after {
+    width: 100%;
+  }
+  &[data-rate='4.5']:after {
+    widows: 90%;
+  }
+  &[data-rate='4']:after {
+    width: 80%;
+  }
+  &[data-rate='3.5']:after {
+    width: 70%;
+  }
+  &[data-rate='3']:after {
+    width: 60%;
+  }
+  &[data-rate='2.5']:after {
+    width: 50%;
+  }
+  &[data-rate='2']:after {
+    width: 40%;
+  }
+  &[data-rate='1.5']:after {
+    width: 30%;
+  }
+  &[data-rate='1']:after {
+    width: 20%;
+  }
+  &[data-rate='0.5']:after {
+    width: 10%;
+  }
+  &[data-rate='0']:after {
+    width: 0%;
+  }
+}
+.review__count {
+  font-size: 12px;
+}
+</style>
--- frontend/components/charts/ChartThumbnail.vue
+++ frontend/components/charts/ChartThumbnail.vue
@@ -1,22 +1,56 @@
 <template>
-<div>
-    <book-thumbnail
-    size="xLarge"
-    src="https://placeimg.com/150/200/any"
-    alt="sample image"
-    ></book-thumbnail>
-    <book-thumbnail-title></book-thumbnail-title>
-    <book-review></book-review>
-</div>
+  <div class="grid-booklist-container">
+    <ul class="grid-booklist">
+      <li v-for="i in 24" :key="i" class="grid-booklist__item">
+        <div class="grid-booklist__rank">{{ i }}</div>
+        <div>
+          <book-thumbnail size="medium" src="https://placeimg.com/150/200/any" alt="sample image"> </book-thumbnail>
+          <book-thumbnail-title :title="title"></book-thumbnail-title>
+          <book-review :rate="parseInt(rate)" :score="parseInt(score)"></book-review>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref } from '@vue/composition-api'
 import BookThumbnail from '../BookThumbnail.vue'
 import BookThumbnailTitle from '../BookThumbnailTitle.vue'
 import BookReview from './BookReview.vue'
 
 export default defineComponent({
-    components: { BookThumbnail, BookReview, BookThumbnailTitle },
+  components: { BookThumbnail, BookReview, BookThumbnailTitle },
+  setup() {
+    const rate = ref(3)
+    const score = ref(3)
+    const title = 'This is Sample Title'
+    return {
+      rate,
+      score,
+      title,
+    }
+  },
 })
 </script>
+
+<style lang="scss" scoped>
+.grid-booklist-container {
+  min-width: 320px;
+  max-width: 800px;
+  margin: 10px auto;
+  .grid-booklist {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 16px 0;
+    .grid-booklist__item {
+      width: 120px;
+      margin: 0 10px 50px 10px;
+      .grid-booklist__rank {
+        text-align: center;
+        margin-bottom: 15px;
+      }
+    }
+  }
+}
+</style>
