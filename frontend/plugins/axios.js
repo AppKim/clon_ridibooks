c90ff4e5082bc08960594b912ecf23e969810ca5
gmldnjs26 <imleesky265@gmail.com>
2022-06-12 21:49:38 +0900

    fix: error

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,18 +1,18 @@
-const baseURL = 'http://localhost:5000'
+const baseURL = 'http://localhost:7070'
 
 const initAxios = ({ $axios }) => {
   $axios.setBaseURL(baseURL)
 
   $axios.onRequest((config) => {
-    console.log(config)
+    // console.log(config)
   })
 
   $axios.onResponse((config) => {
-    console.log(config)
+    // console.log(config)
   })
 
   $axios.onError((e) => {
-    console.log(e.response)
+    // console.log(e.response)
   })
 }
 
