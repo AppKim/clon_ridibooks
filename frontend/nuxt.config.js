cf7595b27e7ac50d289dcafb947e639fd19e7f72
gmldnjs26 <imleesky265@gmail.com>
2022-07-03 18:31:48 +0900

    add plugin

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/repositories', '~/plugins/axios.js'],
+  plugins: ['~/plugins/repositories', '~/plugins/axios.js', '~/plugins/window.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
