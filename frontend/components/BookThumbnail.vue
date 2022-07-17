4e236c74d44be5dfa7482beee1c4fb16bd94f7d2
gmldnjs26 <imleesky265@gmail.com>
2022-07-17 15:16:35 +0900

    fix: bug, styles

--- frontend/components/BookThumbnail.vue
+++ frontend/components/BookThumbnail.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="book-thumbnail">
-    <img :src="src" :alt="alt" :loading="loading" :decoding="decoding" />
-    <span v-show="onShadow" class="shadow"></span>
+  <div class="book-thumbnail" :class="size">
+    <div class="book-thumbnail__contents-wrapper">
+      <img :src="src" :alt="alt" :loading="loading" :decoding="decoding" />
+      <span v-show="onShadow" class="shadow"></span>
+    </div>
   </div>
 </template>
 <script>
@@ -9,6 +11,10 @@ import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
   props: {
+    size: {
+      type: String,
+      default: 'medium',
+    },
     src: {
       type: String,
       required: true,
@@ -36,9 +42,43 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .book-thumbnail {
-  position: relative;
-  display: inline-block;
-  width: inherit;
+  display: flex;
+  align-items: flex-end;
+  &.xLarge {
+    width: 200px;
+    height: 290px;
+    max-height: 290px;
+    @include sp_view {
+      width: 150px;
+      height: 218px;
+      max-height: 218px;
+    }
+  }
+  &.large {
+    width: 140px;
+    height: 216px;
+    max-height: 216px;
+  }
+  &.medium {
+    width: 120px;
+    height: 184px;
+    max-height: 184px;
+    @include sp_view {
+      width: 110px;
+      height: 167px;
+      max-height: 167px;
+    }
+  }
+  &.small {
+    width: 50px;
+    height: 70px;
+    max-height: 70px;
+  }
+  &__contents-wrapper {
+    position: relative;
+    display: inline-block;
+    width: inherit;
+  }
   img {
     display: block;
     width: inherit;
