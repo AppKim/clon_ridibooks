33ee0d600c29d39b01b8078d454b6d3f37904508
gmldnjs26 <imleesky265@gmail.com>
2022-09-04 18:17:19 +0900

    set

--- frontend/components/BookThumbnailLinkGrid.vue
+++ frontend/components/BookThumbnailLinkGrid.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>book-thumbnail-link-grid</div>
+  <div class="book-thumbnail-link-grid">
+    <BookThumbnailLink
+      v-for="(book, i) in bookList"
+      :key="i"
+      size="medium"
+      :src="book.src"
+      :alt="book.alt || book.title"
+      :to="book.link"
+      :title="book.title"
+      :author="book.author"
+      class="book-thumbnail-link-grid__item"
+    />
+  </div>
 </template>
-
 <script>
-export default {}
+import { defineComponent } from '@nuxtjs/composition-api'
+import BookThumbnailLink from './BookThumbnailLink.vue'
+
+export default defineComponent({
+  components: { BookThumbnailLink },
+  props: {
+    bookList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup() {},
+})
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.book-thumbnail-link-grid {
+  overflow: auto;
+  display: grid;
+}
+</style>
