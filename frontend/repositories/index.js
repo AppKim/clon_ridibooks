cfb16b6389e29febfd63cfb9ad1d8cb9a4fa5215
AppKim <jskim2301@yahoo.co.jp>
2022-09-11 18:43:38 +0900

    add charts repository

--- frontend/repositories/index.js
+++ frontend/repositories/index.js
@@ -2,12 +2,14 @@ import { BooksRepository } from './books'
 import { CategoriesRepository } from './categories'
 import { TopRepository } from './top'
 import { CollectionsRepository } from './collections'
+import { ChartsRepository } from './ChartsRepository'
 
 const repositories = {
   top: TopRepository,
   books: BooksRepository,
   categories: CategoriesRepository,
   collections: CollectionsRepository,
+  charts: ChartsRepository,
 }
 
 export const RepositoryFactory = {
