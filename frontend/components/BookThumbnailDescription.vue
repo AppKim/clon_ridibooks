4fcc058fcd0aba1807695183a1142752a52fafd6
gmldnjs26 <imleesky265@gmail.com>
2022-09-12 22:22:29 +0900

    feat: create new-releases page and component

--- frontend/components/BookThumbnailDescription.vue
+++ frontend/components/BookThumbnailDescription.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="book-thumbnail-description" :class="{ 'author-existed': author }">
+  <div class="book-thumbnail-description" :class="styles">
     <div v-show="title" class="book-thumbnail-description__title">
       {{ title }}
     </div>
@@ -9,7 +9,7 @@
   </div>
 </template>
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
+import { computed, defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   props: {
@@ -26,7 +26,19 @@ export default defineComponent({
       default: '',
     },
   },
-  setup() {},
+  setup(props) {
+    const styles = computed(() => {
+      const classes = []
+      if (props.author) {
+        classes.push('author-existed')
+      }
+      classes.push(props.size)
+      return classes.join(' ')
+    })
+    return {
+      styles,
+    }
+  },
 })
 </script>
 
@@ -34,34 +46,6 @@ export default defineComponent({
 .book-thumbnail-description {
   position: relative;
   display: inline-block;
-  & .xLarge {
-    width: 200px;
-    @include sp_view {
-      width: 150px;
-    }
-  }
-  & .large {
-    width: 140px;
-  }
-  & .medium {
-    width: 120px;
-    @include sp_view {
-      width: 110px;
-    }
-  }
-  & .small {
-    width: 50px;
-  }
-  .book-thumbnail-description__title {
-    font-size: 1.3rem;
-    margin-top: 8px;
-    color: $gray;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-  }
   &.author-existed {
     .book-thumbnail-description__title {
       font-size: 1.5rem;
@@ -82,6 +66,34 @@ export default defineComponent({
         font-size: 1.3rem;
       }
     }
+  }
+  &.xLarge {
+    width: 200px;
+    @include sp_view {
+      width: 150px;
+    }
+  }
+  &.large {
+    width: 140px;
+  }
+  &.medium {
+    width: 120px;
+    @include sp_view {
+      width: 110px;
+    }
+  }
+  &.small {
+    width: 50px;
+  }
+  .book-thumbnail-description__title {
+    font-size: 1.3rem;
+    margin-top: 8px;
+    color: $gray;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
   }
 }
 </style>
--- frontend/components/BookThumbnailLinkGrid.vue
+++ frontend/components/BookThumbnailLinkGrid.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>book-thumbnail-link-grid</div>
+  <div class="book-thumbnail-link-grid">
+    <BookThumbnailLink
+      v-for="(book, i) in bookList"
+      :key="i"
+      size="medium"
+      src="https://placeimg.com/150/200/any"
+      :alt="book.alt && book.title && book.title.main"
+      :to="book.link"
+      :title="book.title && book.title.main"
+      :author="book.author"
+      class="book-thumbnail-link-grid__item"
+    />
+  </div>
 </template>
-
 <script>
-export default {}
+import { defineComponent } from '@nuxtjs/composition-api'
+import BookThumbnailLink from './BookThumbnailLink.vue'
+
+export default defineComponent({
+  components: { BookThumbnailLink },
+  props: {
+    bookList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(props) {
+    console.log(props.bookList)
+  },
+})
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.book-thumbnail-link-grid {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 0;
+  margin-bottom: 32px;
+  @include sp_view {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 8px;
+    flex-flow: row wrap;
+  }
+}
+</style>
