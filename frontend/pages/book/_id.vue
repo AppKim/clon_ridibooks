78f87063c6a2a0f6f0c7f5973af9d7a933548a15
gmldnjs26 <imleesky265@gmail.com>
2022-10-10 17:23:25 +0900

    test: vibrant

--- frontend/pages/book/_id.vue
+++ frontend/pages/book/_id.vue
@@ -1,9 +1,38 @@
 <template>
-  <div></div>
+  <div class="book" :style="bgColor">
+    <div class="book__header"></div>
+    <div class="book__main"></div>
+  </div>
 </template>
-
 <script>
-export default {}
+import { computed, defineComponent, onMounted, ref } from '@nuxtjs/composition-api'
+import { useVibrant } from '../../composables/useVibrant'
+
+export default defineComponent({
+  setup() {
+    const { getPaletteData } = useVibrant()
+    const vibrant = ref({})
+    onMounted(async () => {
+      const sampleImgPath = require('@/assets/images/large-sample1.webp')
+      const result = await getPaletteData(sampleImgPath)
+      vibrant.value = result.Vibrant
+    })
+    const bgColor = computed(() => ({
+      background: `rgb(${vibrant.value._rgb.join(',')})`,
+    }))
+    return {
+      bgColor,
+    }
+  },
+})
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.book {
+  min-height: 100vh;
+  &__header {
+  }
+  &__main {
+  }
+}
+</style>
