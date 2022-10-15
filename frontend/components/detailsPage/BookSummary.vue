74cac6bdacd42644b25f6246095da87f57262003
gmldnjs26 <imleesky265@gmail.com>
2022-10-15 21:09:08 +0900

    config: reset components of book-detail page

---
+++ frontend/components/detailsPage/BookSummary.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="book-summary">
+    <BookThumbnail />
+    <div>
+      <CategoriesLink />
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import BookThumbnail from '../BookThumbnail.vue'
+
+export default defineComponent({
+  components: { BookThumbnail },
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/components/detailsPage/CategoryLinkList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="category-link-list"></div>
+</template>
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped></style>
