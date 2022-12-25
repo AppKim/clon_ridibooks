54fa3cfa87cefbf9e59d29169102da8d4db7d8ca
AppKim <jskim2301@yahoo.co.jp>
2022-12-25 17:41:48 +0900

    add myselect checkbox

---
+++ frontend/components/CheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <label>
+    <input :id="id" type="checkbox" :checked="checked" @change="selected" />
+  </label>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+    id: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup(_, { emit }) {
+    const selected = (e) => {
+      emit('change', e.target.id, e.target.checked)
+    }
+    return {
+      selected,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
