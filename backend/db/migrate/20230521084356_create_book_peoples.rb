d7d3eed5227506941e3c41e4c26d316cec541b19
gmldnjs26 <imleesky265@gmail.com>
2023-05-28 18:08:05 +0900

    fix: migration file

--- backend/db/migrate/20230521084356_create_book_peoples.rb
+++
@@ -1,9 +0,0 @@
-class CreateBookPeoples < ActiveRecord::Migration[7.0]
-  def change
-    create_table :book_peoples do |t|
-      t.references :people, null: false, foreign_key: true
-      t.references :book, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
