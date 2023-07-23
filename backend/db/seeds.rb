cd1a3b285d79eae7807c9d8f40c7788d9aa9d0db
AppKim <jskim2301@yahoo.co.jp>
2023-07-23 18:00:13 +0900

    rspec修正エラー

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -18,14 +18,14 @@ end
 
 SEED_DIR = Rails.root.join('db', 'seeds')
 load "#{SEED_DIR}/category/category.rb"
-load "#{SEED_DIR}/book_categories/book_categories.rb"
-load "#{SEED_DIR}/review_comments/review_comments.rb"
 load "#{SEED_DIR}/banner/banner.rb"
 load "#{SEED_DIR}/publisher/publisher.rb"
 load "#{SEED_DIR}/person/person.rb"
 load "#{SEED_DIR}/book/book.rb"
+load "#{SEED_DIR}/book_categories/book_categories.rb"
+load "#{SEED_DIR}/review_comments/review_comments.rb"
 load "#{SEED_DIR}/book/book_detail.rb"
 load "#{SEED_DIR}/book/book_person.rb"
-load "#{SEED_DIR}/banners/banners.rb"
+load "#{SEED_DIR}/banner/banner.rb"
 load "#{SEED_DIR}/selection/selection.rb"
 load "#{SEED_DIR}/book_selection/book_selection.rb"
