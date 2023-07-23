04fbc821a574d390d7a3a98f252b0624b3e4c840
gmldnjs26 <imleesky265@gmail.com>
2023-07-23 17:45:44 +0900

    fix: review

--- backend/db/seeds/person/person.rb
+++ backend/db/seeds/person/person.rb
@@ -1,36 +1,28 @@
 puts 'Person START'
-unless Person.find_by(id: 1)
-  Person.create!(
-    id: 1,
-    person_type: 1, 
-    name: '안젤라 애커만',
-    introduction: 'Writer'
-  )
-end
-unless Person.find_by(id: 2)
-  Person.create!(
-    id: 2,
-    person_type: 1, 
-    name: '에드 마일렛',
-    introduction: 'Writer'
-  )
-end
-unless Person.find_by(id: 3)
-  Person.create!(
-    id: 3,
-    person_type: 2, 
-    name: '최세민',
-    introduction: 'Translator'
-  )
-end
-unless Person.find_by(id: 4)
-  Person.create!(
-    id: 4,
-    person_type: 2, 
-    name: '김흥준',
-    introduction: 'Translator'
-  )
-end
+Person.create!(
+  id: 1,
+  person_type: 1, 
+  name: '안젤라 애커만',
+  introduction: 'Writer'
+)
+Person.create!(
+  id: 2,
+  person_type: 1, 
+  name: '에드 마일렛',
+  introduction: 'Writer'
+)
+Person.create!(
+  id: 3,
+  person_type: 2, 
+  name: '최세민',
+  introduction: 'Translator'
+)
+Person.create!(
+  id: 4,
+  person_type: 2, 
+  name: '김흥준',
+  introduction: 'Translator'
+)
 puts 'Person END'
 
 
--- backend/db/seeds/selection/selection.rb
+++ backend/db/seeds/selection/selection.rb
@@ -1,16 +1,12 @@
 puts 'Selection START'
-unless Selection.find_by(id: 1)
-  Selection.create!(
-    id: 1,
-    title: 'Selection Theme 1'
-  )
-end
-unless Selection.find_by(id: 2)
-  Selection.create!(
-    id: 2,
-    title: 'Selection Theme 2'
-  )
-end
+Selection.create!(
+  id: 1,
+  title: 'Selection Theme 1'
+)
+Selection.create!(
+  id: 2,
+  title: 'Selection Theme 2'
+)
 puts 'Selection END'
 
 
