1737e3dc6610d049e833eb03062b9e45d2bcba7c
AppKim <jskim2301@yahoo.co.jp>
2023-11-05 16:04:54 +0900

    feature: spotlight_api

--- backend/db/seeds/selection/selection.rb
+++ backend/db/seeds/selection/selection.rb
@@ -1,11 +1,13 @@
 puts 'Selection START'
 Selection.create!(
   id: 1,
-  title: 'Selection Theme 1'
+  title: 'Selection Theme 1',
+  selection_type: 'SPOTLIGHT'
 )
 Selection.create!(
   id: 2,
-  title: 'Selection Theme 2'
+  title: 'Selection Theme 2',
+  selection_type: 'COLLECTION'
 )
 puts 'Selection END'
 
