57e751ed18723e664d2cb6ce78abee03b6f665e1
AppKim <jskim2301@yahoo.co.jp>
2023-11-05 17:59:07 +0900

    change: db_column_name_and_type

--- backend/app/models/book_person.rb
+++ backend/app/models/book_person.rb
@@ -3,4 +3,18 @@
 class BookPerson < ApplicationRecord
   belongs_to :book
   belongs_to :person
+
+  def self.authors(book_people)
+    authors = {}
+    book_people.each do | book_person |      
+      authors[book_person.person.person_type] ||= []
+      person = {
+        id: book_person.person.id,
+        name: book_person.person.name
+      }
+      authors[book_person.person.person_type] << person
+    end
+    authors
+  end
+
 end
