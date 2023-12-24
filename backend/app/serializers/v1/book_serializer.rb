b2bc84e7c7930cdcc1b9716d1c28ed91d9635f62
AppKim <jskim2301@yahoo.co.jp>
2023-12-24 17:56:43 +0900

    feature: spotlight api

--- backend/app/serializers/v1/book_serializer.rb
+++ backend/app/serializers/v1/book_serializer.rb
@@ -1,5 +1,5 @@
 class V1::BookSerializer < ActiveModel::Serializer
-    attributes :id, :title, :thumnail, :authors, :publisher
+    attributes :id, :title, :thumnail, :authors, :publisher, :review_summary
 
     def authors
         BookPerson.authors(object.book_people)
@@ -7,5 +7,12 @@ class V1::BookSerializer < ActiveModel::Serializer
     
     def publisher
         object.publisher.name
+    end
+
+    def review_summary
+        ActiveModelSerializers::SerializableResource.new(
+            object,
+            serializer: V1::ReviewSummarySerializer
+        )
     end
 end
