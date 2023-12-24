526b9b7acfd5bda9c565e0bbfe29cc6470394b06
AppKim <jskim2301@yahoo.co.jp>
2023-12-24 17:56:43 +0900

    feature: spotlight api

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
-  has_many :review_comments
+  belongs_to :book
+  has_many :review_comments, dependent: :destroy
+
+  def self.buyer_rating(book)
+    buyer_review(book).pluck(:score)
+  end
+
+  def self.buyer_review(book)
+    book.reviews.filter{ |review| review.is_buyer.eql? true }
+  end
 end
