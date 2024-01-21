cc3c7c9e0a6cf55f782e367475a182d17ae67cf8
AppKim <jskim2301@yahoo.co.jp>
2024-01-21 17:58:27 +0900

    feature: add rspec

---
+++ backend/spec/requests/v1/spotlights_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Spotlights', type: :request do
+    describe 'GET /v1/spotlights' do
+        # let(:spotlights_api) do
+        #     -> { get '/v1/spotlights' }
+        # end
+
+        # # テストテータ作成　
+        # before do
+
+        # end
+
+        # context 'spotlightの本が存在する場合' do
+
+        # end
+
+        # context 'spotlightの本が存在しない場合' do
+
+        # end
+    end
+end
