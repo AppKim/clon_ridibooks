67925b9fe6134d5234d355aa90a849e8a4db34c2
gmldnjs26 <imleesky265@gmail.com>
2024-01-21 18:20:16 +0900

    chore: add comment in home api

--- backend/app/controllers/v1/home_controller.rb
+++ backend/app/controllers/v1/home_controller.rb
@@ -6,7 +6,7 @@ class V1::HomeController < ApplicationController
   private
 
   def serialized_home_data
-    data = { banners: Banner.all, selections: Selection.collection } # TODO:
+    data = { banners: Banner.all, selections: Selection.collection } # TODO: 톱페이지에서 어떤 셀렉션을 습득
     V1::HomeSerializer.new(data).as_json
   end
 end
