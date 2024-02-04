8f87b22d3c517f91e04ae7c80e92a51140d7a713
gmldnjs26 <imleesky265@gmail.com>
2024-02-04 18:53:34 +0900

    fix: ー対応お

--- backend/db/seeds/book_selection/book_selection.rb
+++ backend/db/seeds/book_selection/book_selection.rb
@@ -1,31 +1,12 @@
 puts 'BookSelection START'
-# ID 초기값 설정
-id = 1
 
-# 2부터 7까지의 Selection에 대해 반복
-(1..7).each do |selection_index|
-  # Selection을 찾음
-  selection = Selection.find_by(id: selection_index)
-
-  # Selection이 존재하는 경우에만 처리
-  if selection
-    # 각 Selection에 대해 5권의 Book을 맵핑
-    (1..6).each do |book_index|
-      # Book을 찾음
-      book = Book.find_by(id: book_index)
-
-      # Book이 존재하는 경우에만 BookSelection 생성
-      if book
-        BookSelection.create!(
-          id: id,
-          book_id: book.id,
-          selection_id: selection.id
-        )
-
-        # ID 값을 증가
-        id += 1
-      end
-    end
+Selection.find_each do |selection|
+  Book.find_each do |book|
+    BookSelection.create!(
+      book_id: book.id,
+      selection_id: selection.id
+    )
   end
 end
+
 puts 'BookSelection END'
