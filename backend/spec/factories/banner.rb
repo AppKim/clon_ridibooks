7bd4b55b7adbc405b895aa2833e18b5d1098c9c2
gmldnjs26 <imleesky265@gmail.com>
2024-06-02 17:46:02 +0900

    fix: レビュー対応

--- backend/spec/factories/banner.rb
+++ backend/spec/factories/banner.rb
@@ -3,5 +3,9 @@ FactoryBot.define do
         image_url { "https://picsum.photos/200" }
         link_url { "http://localhost:8080/select" }
         title { "미리 보는 6월 업데이트" }
+
+        trait :fixed_id do
+            id { 1 }
+        end
     end
 end
