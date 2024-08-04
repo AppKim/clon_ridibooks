472fdc3c90d46da4fd8b81864bea95c81fcc194a
AppKim <jskim2301@yahoo.co.jp>
2024-08-04 16:29:23 +0900

    selectionの概念を廃止

--- backend/app/models/collection.rb
+++ backend/app/models/collection.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Selection < ApplicationRecord
-  has_many :book_selections
-  has_many :books, through: :book_selections
+  has_many :book_collections
+  has_many :books, through: :book_collections
 
   scope :spotlight, -> { find_by(selection_type: 'SPOTLIGHT') }
   scope :collection, -> { where(selection_type: 'COLLECTION') }
