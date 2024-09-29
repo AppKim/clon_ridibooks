e8d46d16a0e9cfd5757fcf92a874a80fcc92eea0
gmldnjs26 <imleesky265@gmail.com>
2024-09-29 17:49:09 +0900

    fix: review schema

--- backend/db/migrate/20240929082240_modify_reviews_and_review_comments.rb
+++ backend/db/migrate/20240929082240_modify_reviews_and_review_comments.rb
@@ -1,6 +1,9 @@
 class ModifyReviewsAndReviewComments < ActiveRecord::Migration[6.1]
   def change
     change_table :reviews do |t|
+      remove_foreign_key :reviews, column: :review_comment_id
+      remove_index :reviews, :review_comment_id
+      t.remove :review_comment_id # 기존의 review_comment_id 필드 삭제
       t.text :content, null: true, comment: "レビュー内容"
       t.boolean :has_spoiler, null: false, default: false
     end
