4708b97d704e984ad592ae04bb1e5d39125a46c1
gmldnjs26 <imleesky265@gmail.com>
2024-09-29 17:42:31 +0900

    fix: review schema

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -2,7 +2,7 @@
 
 class Review < ApplicationRecord
   belongs_to :book
-  has_many :review_comments, dependent: :destroy
+  has_many :review_comments
 
   def self.buyer_rating(book)
     buyer_review(book).pluck(:score)
