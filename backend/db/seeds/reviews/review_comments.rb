4fba468d5986424b0a246b8a98d0ff052ab31f1c
gmldnjs26 <imleesky265@gmail.com>
2024-12-08 17:33:21 +0900

    fix: review seed

--- backend/db/seeds/reviews/review_comments.rb
+++ backend/db/seeds/reviews/review_comments.rb
@@ -1,6 +1,18 @@
 puts 'review_comments START'
 ReviewComment.create!(
-    comment: 'Good Story',
-    has_spoiler: true
-  )
+    review_id: Review.first.id,
+    content: 'review comment1'
+)
+ReviewComment.create!(
+    review_id: Review.first.id,
+    content: 'review comment2'
+)
+ReviewComment.create!(
+    review_id: Review.second.id,
+    content: 'review comment3'
+)
+ReviewComment.create!(
+    review_id: Review.second.id,
+    content: 'review comment4'
+)
 puts 'review_comments END'
